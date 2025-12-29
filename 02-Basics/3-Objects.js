024dd850f2d78a03f127566d3e343a9abfd78db7
mmuqtasid <96142528+mmuqtasid@users.noreply.github.com>
2025-12-29 12:04:20 +0500

    Objects

---
+++ 02-Basics/3-Objects.js
@@ -0,0 +1,10 @@
+const person = {
+    name : "Muqtasid",
+    age : "20",
+    location : "isb" , 
+    email : "user@gmail.com",
+    lastLoginday : ["Sunday" , "Monday"],
+
+}
+console.log(person.name);
+console.log(person["age"]);
