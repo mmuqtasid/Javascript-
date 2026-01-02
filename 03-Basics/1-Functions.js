1ade596cf6579d08a163ef5789e60bae38a1b774
mmuqtasid <96142528+mmuqtasid@users.noreply.github.com>
2026-01-02 12:49:27 +0500

    Functions

---
+++ 03-Basics/1-Functions.js
@@ -0,0 +1,16 @@
+//Functions
+function userList() {
+console.log("userOne");
+console.log("userTwo");
+console.log("userThree");
+}
+
+userList();
+
+
+//Add Two Num
+
+function AddTwoNum (numOne , numTwo){
+    console.log(numOne + numTwo);
+}
+AddTwoNum(2 , 4 );
