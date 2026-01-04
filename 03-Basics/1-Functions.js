af47e897151c6af0c26f048ffa06d322dfbfc04f
mmuqtasid <96142528+mmuqtasid@users.noreply.github.com>
2026-01-04 18:18:01 +0500

    Changes

--- 03-Basics/1-Functions.js
+++ 03-Basics/1-Functions.js
@@ -31,3 +31,39 @@ return `${username} just loggedin`;
 }
 
 console.log(useLoginMessage())
+
+
+//Shopping cart example
+
+function calculateCartPrice(...num1){
+
+   
+    return num1
+
+}
+
+ console.log(calculateCartPrice());
+
+ const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
