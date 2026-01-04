fb149519baf6c10f63dfdc2c41e17b9a0080e803
mmuqtasid <96142528+mmuqtasid@users.noreply.github.com>
2026-01-04 17:08:27 +0500

    functions

--- 03-Basics/1-Functions.js
+++ 03-Basics/1-Functions.js
@@ -14,3 +14,20 @@ function AddTwoNum (numOne , numTwo){
     console.log(numOne + numTwo);
 }
 AddTwoNum(2 , 4 );
+
+
+
+
+function addTwoNumer(){
+
+}
+
+function useLoginMessage(username){
+    if(username === undefined){
+        console.log("Please Enter UserName")
+return;
+}
+return `${username} just loggedin`;
+}
+
+console.log(useLoginMessage())
