c9030e677bc566c5e239edcb0dfded201f129279
mmuqtasid <96142528+mmuqtasid@users.noreply.github.com>
2026-01-12 15:55:24 +0500

    Loops

--- 05-Iteration/1-For_Loop.js
+++ 05-Iteration/1-For_Loop.js
@@ -1,49 +1,52 @@
 // for
-
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    if (element == 5) {
-        //console.log("5 is best number");
-    }
-    //console.log(element);
-    
-}
-
-// console.log(element);
-
-for (let i = 1; i <= 10; i++) {
-    //console.log(`Outer loop value: ${i}`);
-   for (let j = 1; j <= 10; j++) {
-    //console.log(`Inner loop value ${j} and inner loop ${i}`);
-    //console.log(i + '*' + j + ' = ' + i*j );
-   }
-    
-}
-let myArray = ["flash", "batman", "superman"]
-//console.log(myArray.length);
-for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    //console.log(element);
-    
+for (let i = 0 ; i <= 10 ; i++) {
+    console.log(i)
 }
 
 
-// break and continue
-
-// for (let index = 1; index <= 20; index++) {
-//     if (index == 5) {
-//         console.log(`Detected 5`);
-//         break
+// for (let j = 0 ; j < 10 ; j++) {
+//     for (let k = 0 ; k < 10 ; k++){
+//         console.log(j*k);
 //     }
-//    console.log(`Value of i is ${index}`);
+// }
+// let myArray = ["flash", "batman", "superman"]
+// //console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     //console.log(element);
     
 // }
 
-for (let index = 1; index <= 20; index++) {
-    if (index == 5) {
-        console.log(`Detected 5`);
-        continue
-    }
-   console.log(`Value of i is ${index}`);
+
+// for(let i = 0 ; i <= 10 ; i++) {
+//     console.log(`Outer Loop Value: ${i}`);
+//     for (let j = 0 ; j <= 10 ; j++);
+//     {
+//         console.log(i + '*' + j + '=' + i*j);
+//     }
+// }
+
+// let myArr = ["flash" , "Batmen" , "supermen"];
+// console.log(myArr.length)
+// for (let index = 0; index < myArr.length; index++) {
+//     const element = myArr[index];
+//     console.log(element);
     
+// }
+
+//Brake Continue
+for (let index = 1 ; index <= 20 ; index++){
+    if (index == 5 ){
+        console.log("Detected");
+        break;
+    }
+    console.log(`value of index ${index}`)
+}
+
+for (let index = 1 ; index <= 20 ; index++){
+    if (index == 5 ){
+        console.log("Detected");
+        continue;
+    }
+    console.log(`value of index ${index}`)
 }
---
+++ 05-Iteration/2-Loops.js
@@ -0,0 +1,5 @@
+//While loops and Do While Loop 
+while(userList <= 10) {
+    console.log(userList);
+    userList = userList + 1;
+}
